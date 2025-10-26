5e7a98434a46ae885d6968d3249fff6c4768ac83
gorgeousanya <aplokhotnyuk@rambler.ru>
2025-10-26 18:55:13 +0300

    task 4

--- src/features/RHF/model/initialValues.ts
+++ src/features/RHF/model/initialValues.ts
@@ -3,7 +3,7 @@ import type { FormValues } from './types';
 export const createInitialsValues = (values?: FormValues): FormValues => ({
     username: values?.username ?? '',
     password: values?.password ?? '',
-    password2: values?.password ?? '',
+    password2: values?.password2 ?? '',
     email: values?.email ?? '',
     media: values?.media ?? [{ value: '' }],
 });
